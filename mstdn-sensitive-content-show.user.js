5230762f44a582c5e40e2ee07c8496aee8116e18
adcin2k17 <LUL@example.com>
2017-04-22 20:56:43 +0900

    Changes to be committed: 	modified:   mstdn-sensitive-content-show.user.js ゆーざーじすのあれ書き足すの忘れてた

--- mstdn-sensitive-content-show.user.js
+++ mstdn-sensitive-content-show.user.js
@@ -1,8 +1,22 @@
-setInterval(function(){$(".status").map(function(){
+// ==UserScript==
+// @name         mstdn-sensitive-content-show
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  ますとどんの不適切なコンテンツを表示を勝手に表示するやつ
+// @author       karubabu
+// @match        https://mstdn.maud.io/web/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setInterval(function(){$(".status").map(function(){
 	if($(this).children("div").length == 4)
     {
 		var that = this;
-		if($(that).find("div > div > a:not('[class]')").length == 0){$(that).children("div").eq(2).find("div").trigger("click")}
+		if($(that).find("div > div > a:not('[class]')").length == 0){$(that).children("div").eq(2).find("div").trigger("click");}
 
 }
-})},1000);
+});},1000);
+
+})();
